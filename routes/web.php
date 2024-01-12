a963775f17f31a662e20582dd21a10302621277c
Ecos1213 <diasflac1213@gmail.com>
2024-01-11 20:46:18 -0500

    Agrega rutas y explicacion de la libreria request que en este caso seria parametros de url que podemos tomar de la libreria request

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,30 @@
 <?php
 
+
+use Illuminate\Http\Request; // tambien podemos controlar y manejar lo enviado por un usuario, para eso usamos la librearia request de laravel
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
+/**
+* Route::get    | Consultar
+* Route::post   | Guardar
+* Route::delete | Eliminar
+* Route::put    | Actualizar
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return "ruta home";
 });
+
+Route::get('blog', function () {
+    return "Listado de publicaciones";
+});
+
+Route::get('blog/{slug}', function ($slug) { // resivimos un parametro llamado slug, este parametro tiene que definirse en el metodo como se escribe en la ruta
+    // consulta a base de datos
+    return $slug;
+});
+
+Route::get('buscar', function (Request $request) { // despues de llamar la libreria con use colocamos un argumento tipo objeto request para tomar los metodos que vienen definidas en laravel
+    return $request->all(); // usamos el metodo all del objeto request para tomar todas las peticiones enviadas por el usuario, esto quiere decir que si escribimos http://127.0.0.1:8000/buscar?query=php tomara como parametro query y el valor php para eso sirve en este caso el request
+});
+
