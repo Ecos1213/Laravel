78d20f905f60822869805a4bbaa46a8b9615b261
Ecos1213 <diasflac1213@gmail.com>
2024-01-13 10:42:32 -0500

    Agrega index.blade.php y un nuevo controlador donde la vista apunta a posts.index

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(PageController::class)->group(function() {
@@ -22,5 +23,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::resource('posts', PostController::class)->except('show')->middleware(['auth', 'verified']);
 
 require __DIR__.'/auth.php';
